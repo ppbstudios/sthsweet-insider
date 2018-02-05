560b80045966b5b4edb4188c57b227b7e3ea522a
David HoYean Hwang <hoyean.hwang@ppbstudios.com>
2018-02-05 10:56:27 +0900

    insider object for sthsweet

---
+++ gtm_insider_tag.js
@@ -0,0 +1,11 @@
+<script>
+  window.insider_object = {
+    version: "0.9.2",
+	page: {{Insider_Page}},
+    user: {{Insider_User}},
+    product:{{Insider_Product}},
+    basket:{{Insider_Basket}},
+    transaction: {{Insider_Transaction}},
+    listing: {{Insider_Listing}}
+}
+</script>
